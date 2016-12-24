34231b92d1603ffcd9c7e4d4427a47f0ba6ed46e
SirArkimedes <sirarkimedes@gmail.com>
2016-12-24 00:45:23 -0600

    Add other textfields
    Attempt to center textfields when keyboard pops.
    Does not work 100%. Also doesn’t have an unanimate.

--- ThePost/SignUpViewController.swift
+++ ThePost/SignUpViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: RoundedTextField!
+    @IBOutlet weak var emailTextField: RoundedTextField!
+    @IBOutlet weak var passwordTextField: RoundedTextField!
+    @IBOutlet weak var confirmPasswordTextField: RoundedTextField!
     
     private var usernameImageView:UIImageView!
+    private var emailImageView:UIImageView!
+    private var passwordImageView:UIImageView!
+    private var confirmPasswordImageView:UIImageView!
+    
+    @IBOutlet weak var redIndicator: UIView!
+    @IBOutlet weak var yellowIndicator: UIView!
+    @IBOutlet weak var greenIndicator: UIView!
+    
+    @IBOutlet weak var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         usernameImageView = UIImageView(image: UIImage(named: "UsernameAvatar")!.withRenderingMode(.alwaysTemplate))
         formatTextField(field: usernameTextField, withImageView: usernameImageView)
+        
+        emailImageView = UIImageView(image: UIImage(named: "Mail")!.withRenderingMode(.alwaysTemplate))
+        formatTextField(field: emailTextField, withImageView: emailImageView)
+        
+        passwordImageView = UIImageView(image: UIImage(named: "Password")!.withRenderingMode(.alwaysTemplate))
+        formatTextField(field: passwordTextField, withImageView: passwordImageView)
+        
+        confirmPasswordImageView = UIImageView(image: UIImage(named: "ConfirmPasswordCheck")!.withRenderingMode(.alwaysTemplate))
+        formatTextField(field: confirmPasswordTextField, withImageView: confirmPasswordImageView)
+        
+        redIndicator.layer.cornerRadius = 10.0
+        yellowIndicator.layer.cornerRadius = 10.0
+        greenIndicator.layer.cornerRadius = 10.0
+        
+        signUpButton.layer.cornerRadius = 25.0
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
     }
     
     // MARK: - Helpers
@@ -35,8 +64,30 @@ class SignUpViewController: UIViewController {
         }
         imageView.contentMode = .left
         
-        usernameTextField.rightViewMode = .always
-        usernameTextField.rightView = imageView
+        field.rightViewMode = .always
+        field.rightView = imageView
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! TimeInterval
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! NSNumber
+        let keyboardFrame = notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! CGRect
+        let keyboardUpCenterY = (view.frame.height - keyboardFrame.height) / 2.0
+        
+        let textFieldsCenterY = usernameTextField.frame.origin.y + ((confirmPasswordTextField.frame.origin.y) - usernameTextField.frame.origin.y) / 2.0
+        
+        for otherView in view.subviews {
+            if otherView is UITextField {
+                let centerOffset = keyboardUpCenterY - (textFieldsCenterY - otherView.frame.origin.y)
+                UIView.animate(withDuration: duration, delay: 0.0, options: UIViewAnimationOptions(rawValue: UInt(curve)), animations: {
+                    otherView.frame = CGRect(x: otherView.frame.origin.x, y: centerOffset, width: otherView.frame.width, height: otherView.frame.height)
+                }, completion: nil)
+            } else {
+                UIView.animate(withDuration: duration, delay: 0.0, options: UIViewAnimationOptions(rawValue: UInt(curve)), animations: {
+                    otherView.alpha = 0.0
+                }, completion: nil)
+            }
+        }
     }
 
 }
