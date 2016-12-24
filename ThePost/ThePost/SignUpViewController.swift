c8973386e95b51b4f0d1154b48456eb2bca12748
SirArkimedes <sirarkimedes@gmail.com>
2016-12-23 20:48:51 -0600

    Switch out ViewController for SignUp

--- ThePost/ThePost/SignUpViewController.swift
+++ ThePost/ThePost/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  ThePost
 //
 //  Created by Andrew Robinson on 12/23/16.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
