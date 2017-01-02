a94b2a979c80318bb0c0c690375b26b0b619202e
SirArkimedes <sirarkimedes@gmail.com>
2017-01-02 12:10:12 -0600

    Remove observers from product listing page
    Also removed the fake data generation, as this is no longer needed.

--- ThePost/ProductListingViewController.swift
+++ ThePost/ProductListingViewController.swift
@@ -18,7 +18,7 @@ class ProductListingViewController: UIViewController, UICollectionViewDataSource
     private var products: [Product] = []
     
     private var ref: FIRDatabaseReference!
-    private var productRef: FIRDatabaseReference!
+    private var productRef: FIRDatabaseReference?
     
     // MARK: - View lifecycle
     
@@ -26,7 +26,6 @@ class ProductListingViewController: UIViewController, UICollectionViewDataSource
         super.viewDidLoad()
         
         ref = FIRDatabase.database().reference()
-        productRef = ref.child("products")
                 
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: floor(view.frame.width * (190/414)), height: floor(view.frame.height * (235/736)))
@@ -37,70 +36,55 @@ class ProductListingViewController: UIViewController, UICollectionViewDataSource
             navigationController!.navigationBar.topItem!.title = name + " \(start)-\(end)"
         }
         
-        // TODO: Remove
-        addFakeData()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        products.removeAll()
-        productRef.observe(.childAdded, with: { snapshot in
-            if let productDict = snapshot.value as? [String: Any] {
-                if let jeepModel = JeepModel.enumFromString(string: productDict["jeepModel"] as! String) {
-                    if let condition = Condition.enumFromString(string: productDict["condition"] as! String) {
-                        let product = Product(withName: productDict["name"] as! String,
-                                              model: jeepModel,
-                                              price: productDict["price"] as! Float,
-                                              condition: condition)
-                        self.products.append(product)
-                        self.collectionView.reloadData()
-                    }
-                }
-            }
-        })
+        super.viewWillAppear(animated)
         
-        productRef.observe(.childRemoved, with: { snapshot in
-            if let productDict = snapshot.value as? [String: Any] {
-                if let jeepModel = JeepModel.enumFromString(string: productDict["jeepModel"] as! String) {
-                    if let condition = Condition.enumFromString(string: productDict["condition"] as! String) {
-                        let product = Product(withName: productDict["name"] as! String,
-                                              model: jeepModel,
-                                              price: productDict["price"] as! Float,
-                                              condition: condition)
-                        
-                        let index = self.indexOfMessage(product)
-                        self.products.remove(at: index)
-                        
-                        self.collectionView.reloadData()
+        if productRef == nil {
+            productRef = ref.child("products")
+            
+            productRef!.observe(.childAdded, with: { snapshot in
+                if let productDict = snapshot.value as? [String: Any] {
+                    if let jeepModel = JeepModel.enumFromString(string: productDict["jeepModel"] as! String) {
+                        if let condition = Condition.enumFromString(string: productDict["condition"] as! String) {
+                            let product = Product(withName: productDict["name"] as! String,
+                                                  model: jeepModel,
+                                                  price: productDict["price"] as! Float,
+                                                  condition: condition)
+                            self.products.append(product)
+                            self.collectionView.reloadData()
+                        }
                     }
                 }
-            }
-        })
+            })
+            
+            productRef!.observe(.childRemoved, with: { snapshot in
+                if let productDict = snapshot.value as? [String: Any] {
+                    if let jeepModel = JeepModel.enumFromString(string: productDict["jeepModel"] as! String) {
+                        if let condition = Condition.enumFromString(string: productDict["condition"] as! String) {
+                            let product = Product(withName: productDict["name"] as! String,
+                                                  model: jeepModel,
+                                                  price: productDict["price"] as! Float,
+                                                  condition: condition)
+                            
+                            let index = self.indexOfMessage(product)
+                            self.products.remove(at: index)
+                            
+                            self.collectionView.reloadData()
+                        }
+                    }
+                }
+            })
+        }
     }
     
-    private func addFakeData() {
-        let product1 = Product(withName: "This is my part. :D", model: jeepModel.type, price: 40.24, condition: .new)
-        product1.images.append(#imageLiteral(resourceName: "ProductSample1"))
-        products.append(product1)
+    deinit {
+        ref.removeAllObservers()
         
-        let product2 = Product(withName: "Jeep Bundle", model: jeepModel.type, price: 683.0, condition: .used)
-        product2.images.append(#imageLiteral(resourceName: "ProductSample1"))
-        products.append(product2)
-        
-        let product5 = Product(withName: "My Jeep", model: jeepModel.type, price: 11000.0, condition: .broke)
-        product5.images.append(#imageLiteral(resourceName: "ProductSample2"))
-        product5.images.append(#imageLiteral(resourceName: "ProductSample1"))
-        product5.images.append(#imageLiteral(resourceName: "ProductSample2"))
-        product5.images.append(#imageLiteral(resourceName: "ProductSample1"))
-        products.append(product5)
-        
-        let product3 = Product(withName: "Jeep Wrangler JK soft top parts 2 door OEM mopar", model: jeepModel.type,  price: 175.0, condition: .remanufactured)
-        product3.images.append(#imageLiteral(resourceName: "ProductSample2"))
-        products.append(product3)
-        
-        let product4 = Product(withName: "Custom spoked rims", model: jeepModel.type, price: 1000.0, condition: .other)
-        product4.images.append(#imageLiteral(resourceName: "ProductSample1"))
-        products.append(product4)
+        if let productRef = productRef {
+            productRef.removeAllObservers()
+        }
     }
     
     // MARK: - CollectionView datasource
