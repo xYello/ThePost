451fb387dc9a490ed210f44052fced9dead5257a
SirArkimedes <sirarkimedes@gmail.com>
2017-01-06 11:02:16 -0600

    Add GeoFire pod
    This pulls from the github repo. Not the actual pod.

--- Podfile
+++ Podfile
@@ -14,6 +14,9 @@ target 'ThePost' do
   pod 'Firebase/Database'
   pod 'FirebaseUI'
 
+  # GeoFire
+  pod 'GeoFire', :git => 'https://github.com/firebase/geofire-objc.git'
+
   # Fabric
   pod 'Crashlytics'
 
---
+++ Pods/Target Support Files/GeoFire/GeoFire-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GeoFire.h"
+#import "GFCircleQuery.h"
+#import "GFQuery.h"
+#import "GFRegionQuery.h"
+#import "GeoFire+Private.h"
+#import "GFBase32Utils.h"
+#import "GFGeoHash.h"
+#import "GFGeoHashQuery.h"
+#import "GFQuery+Private.h"
+
+FOUNDATION_EXPORT double GeoFireVersionNumber;
+FOUNDATION_EXPORT const unsigned char GeoFireVersionString[];
+
