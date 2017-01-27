5398b211eeb08aa013563e2cf8df09f85af8f4b7
SirArkimedes <sirarkimedes@gmail.com>
2017-01-26 18:11:12 -0600

    Adjust textfields on sign in view controller
    When tapping anywhere in the view, the keyboard will be dismissed.

--- ThePost/SignInViewController.swift
+++ ThePost/SignInViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var errorLabel: UILabel!
     
@@ -19,12 +19,24 @@ class SignInViewController: UIViewController {
     
     @IBOutlet weak var signInButton: UIButton!
     
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var emailTextFieldCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var passwordTextFieldCenterYConstraint: NSLayoutConstraint!
+    
     private var emailImageView: UIImageView!
     private var passwordImageView: UIImageView!
+    
+    private var isTypingInTextField = false
     
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
         
         errorLabel.isHidden = true
         
@@ -39,6 +51,37 @@ class SignInViewController: UIViewController {
         signInButton.roundCorners()
     }
     
+    // MARK: - Textfield delegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if !isTypingInTextField {
+            isTypingInTextField = true
+            
+            emailTextFieldCenterYConstraint.constant = emailTextFieldCenterYConstraint.constant - nameView.frame.height + 28
+            passwordTextFieldCenterYConstraint.constant = passwordTextFieldCenterYConstraint.constant - nameView.frame.height + 28
+            
+            UIView.animate(withDuration: 0.25, animations: {
+                self.view.layoutIfNeeded()
+                self.nameView.alpha = 0.0
+            })
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if !isTypingInTextField {
+        
+            emailTextFieldCenterYConstraint.constant = emailTextFieldCenterYConstraint.constant + nameView.frame.height - 28
+            passwordTextFieldCenterYConstraint.constant = passwordTextFieldCenterYConstraint.constant + nameView.frame.height - 28
+            
+            UIView.animate(withDuration: 0.25, animations: {
+                self.view.layoutIfNeeded()
+                self.nameView.alpha = 1.0
+            })
+        }
+    }
+    
     // MARK: - Actions
     
     @IBAction func forgotPasswordButtonPressed(_ sender: UIButton) {
@@ -49,9 +92,15 @@ class SignInViewController: UIViewController {
         if let email = emailTextField.text, let password = passwordTextField.text {
             FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { user, error in
                 if let error = error {
-                    if error.localizedDescription == "The password is invalid or the user does not have a password." {
+                    if error.localizedDescription == "The email address is badly formatted." {
                         self.errorLabel.isHidden = false
-                        self.errorLabel.text = "Invalid credentials. Please try again."
+                        self.errorLabel.text = "Invalid email address. Please try again."
+                    } else if error.localizedDescription == "The password is invalid or the user does not have a password." {
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = "Invalid email or password. Please try again."
+                    } else if error.localizedDescription == "There is no user record corresponding to this identifier. The user may have been deleted." {
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = "Invalid email or password. Please try again."
                     } else if error.localizedDescription == "We have blocked all requests from this device due to unusual activity. Try again later." {
                         self.errorLabel.isHidden = false
                         self.errorLabel.text = "Please stop."
@@ -72,6 +121,11 @@ class SignInViewController: UIViewController {
     
     @IBAction func twitterButtonPressed(_ sender: UIButton) {
         
+    }
+    
+    @objc private func dismissKeyboard() {
+        isTypingInTextField = false
+        view.endEditing(false)
     }
     
     // MARK: - Helpers
