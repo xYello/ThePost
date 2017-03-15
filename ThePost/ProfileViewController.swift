0411737040865f1b39c51e481efc4448ad19490c
Michael Blades <michaelblades12@gmail.com>
2017-03-14 22:13:41 -0500

    Updated ProfileView

--- ThePost/ProfileViewController.swift
+++ ThePost/ProfileViewController.swift
@@ -18,21 +18,24 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         case liked
     }
     
-    @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var profileNameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var settingsButton: UIButton!
     
+    @IBOutlet weak var buildTrustView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var farLeftStar: UIImageView!
     @IBOutlet weak var leftMidStar: UIImageView!
     @IBOutlet weak var midStar: UIImageView!
     @IBOutlet weak var rightMidStar: UIImageView!
     @IBOutlet weak var farRightStar: UIImageView!
+    @IBOutlet weak var badgeLabel: UILabel!
     
     @IBOutlet weak var numberOfReviewsLabel: UILabel!
     
     @IBOutlet weak var sellingProductTypeButton: UIButton!
     @IBOutlet weak var bottomMostSeperator: UIView!
+    @IBOutlet weak var badgeView: UIView!
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -56,6 +59,8 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     private var userProductsRef: FIRDatabaseReference?
     private var likesQuery: FIRDatabaseQuery?
+    
+    private var badgeColor: UIColor = #colorLiteral(red: 0.9600599408, green: 0.6655590534, blue: 0.09231746942, alpha: 1)
     
     private var shouldUpdateProfileOnNextView = false
     
@@ -94,6 +99,56 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileImageView.roundCorners()
+        profileImageView.clipsToBounds = true
+        
+        buildTrustView.roundCorners(radius: 5.0)
+        buildTrustView.clipsToBounds = true
+        
+        
+        //// Rectangle Drawing
+        let rectanglePath = UIBezierPath()
+        rectanglePath.move(to: CGPoint(x: -26, y: 10))
+        rectanglePath.addLine(to: CGPoint(x: 19, y: 10))
+        rectanglePath.addLine(to: CGPoint(x: 19, y: -8))
+        rectanglePath.addLine(to: CGPoint(x: -26, y: -8))
+        rectanglePath.addLine(to: CGPoint(x: -26, y: 10))
+        badgeColor.setFill()
+        rectanglePath.fill()
+        let rectangleShape = CAShapeLayer()
+        rectangleShape.path = rectanglePath.cgPath
+        rectangleShape.fillColor = badgeColor.cgColor
+        rectangleShape.position = CGPoint(x: profileNameLabel.center.x - profileNameLabel.bounds.width/2 - rectangleShape.bounds.width, y: profileNameLabel.center.y)
+        badgeView.layer.addSublayer(rectangleShape)
+        
+        
+        //// Polygon Drawing
+        let polygonPath = UIBezierPath()
+        polygonPath.move(to: CGPoint(x: 28, y: 0.79))
+        polygonPath.addLine(to: CGPoint(x: 20.34, y: 10))
+        polygonPath.addLine(to: CGPoint(x: 6.94, y: 7.37))
+        polygonPath.addLine(to: CGPoint(x: 6.66, y: -5.43))
+        polygonPath.addLine(to: CGPoint(x: 20.34, y: -8))
+        polygonPath.addLine(to: CGPoint(x: 28, y: 0.79))
+        polygonPath.close()
+        badgeColor.setFill()
+        polygonPath.fill()
+        let polyShape = CAShapeLayer()
+        polyShape.path = polygonPath.cgPath
+        polyShape.fillColor = badgeColor.cgColor
+        polyShape.position = CGPoint(x: rectangleShape.position.x + rectangleShape.bounds.width / 2, y: 100)
+        badgeView.layer.addSublayer(polyShape)
+        
+        
+        rectangleShape.bounds.size = CGSize.init(width: profileNameLabel.bounds.width, height: profileNameLabel.bounds.height)
+        polyShape.bounds.size = CGSize.init(width: profileNameLabel.bounds.width, height: profileNameLabel.bounds.height)
+        
+        rectangleShape.position = CGPoint(x: badgeView.center.x, y: badgeView.center.y - rectangleShape.bounds.size.height / 2)
+        polyShape.position = CGPoint(x: badgeView.center.x, y: rectangleShape.position.y)
+        
+        badgeView.bringSubview(toFront: badgeLabel)
+        
+        
         
         var uid = ""
         if let id = userId {
