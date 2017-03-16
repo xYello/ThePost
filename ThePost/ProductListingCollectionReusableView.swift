48afe151be6f85bcfe0332c8c2f2acada27dd0ab
Tyler Flowers <tylerflowers@me.com>
2017-03-15 23:08:55 -0700

    idk what's going on here

---
+++ ThePost/ProductListingCollectionReusableView.swift
@@ -0,0 +1,16 @@
+//
+//  ProductListingCollectionReusableView.swift
+//  ThePost
+//
+//  Created by Andrew Robinson on 3/10/17.
+//  Copyright © 2017 The Post. All rights reserved.
+//
+
+import UIKit
+
+class ProductListingCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var filterButton: UIButton!
+    
+}
