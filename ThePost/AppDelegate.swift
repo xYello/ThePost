ab6678affc4d8f9b1583a24608d472f4902d1bf0
SirArkimedes <sirarkimedes@gmail.com>
2017-03-18 08:55:43 -0500

    Enable database persistence
    Determined that extra offline data management was unnecessary. It may be necessary for some things in the future, but that this point in time, it is not needed; since the app can still function even while the device is offline.

--- ThePost/AppDelegate.swift
+++ ThePost/AppDelegate.swift
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
+        
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         Fabric.with([Answers.self, Crashlytics.self, Twitter.self])
         
