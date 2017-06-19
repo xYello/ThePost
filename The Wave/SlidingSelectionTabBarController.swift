5c5fabc65025976c14087ae1d9b6f3ca08194220
SirArkimedes <sirarkimedes@gmail.com>
2017-06-18 22:52:52 -0500

    Update Tab bar icons to match spec sheet

--- The Wave/SlidingSelectionTabBarController.swift
+++ The Wave/SlidingSelectionTabBarController.swift
@@ -48,6 +48,7 @@ class SlidingSelectionTabBarController: UITabBarController, UITabBarControllerDe
             
             for item in tabBar.items! {
                 item.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+                item.image = item.image?.withRenderingMode(.alwaysOriginal)
             }
         }
         
