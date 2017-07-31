51afa8e1e5ea2b72f50f117303914d4298b370c1
Andrew Robinson <sirarkimedes@gmail.com>
2017-07-30 22:41:24 -0500

    Finish the camera picture taker

--- The Wave/Image selector/ImageSelectorViewController.swift
+++ The Wave/Image selector/ImageSelectorViewController.swift
@@ -9,19 +9,52 @@
 import UIKit
 import Photos
 import AVFoundation
+import TOCropViewController
 
 class ImageSelectorViewController: UIViewController {
 
-    @IBOutlet weak var controlsContainer: UIView!
-    @IBOutlet weak var savedImagesButton: UIButton!
+    fileprivate enum CameraState {
+        case takingImage
+        case tookImage
+        case choseImageFromPicker
+    }
 
+    @IBOutlet weak var photoPreviewImageView: UIImageView!
+    @IBOutlet weak var controlsContainer: UIView!
+    @IBOutlet weak var pictureTakeButton: UIButton!
+    @IBOutlet weak var savedImagesButton: UIButton!
+    @IBOutlet weak var uploadPictureButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
     private var session: AVCaptureSession!
     fileprivate var photoOutput: AVCapturePhotoOutput!
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+
+    fileprivate var state: CameraState! {
+        didSet {
+            if state == .takingImage {
+                photoPreviewImageView.contentMode = .scaleAspectFill
+                photoPreviewImageView.isHidden = true
+
+                cancelButton.setTitle("Cancel", for: .normal)
+
+                pictureTakeButton.isHidden = false
+                savedImagesButton.isHidden = false
+                uploadPictureButton.isHidden = true
+            } else {
+                photoPreviewImageView.isHidden = false
+                cancelButton.setTitle(state == .tookImage ? "Retake" : "Rechoose", for: .normal)
+                pictureTakeButton.isHidden = true
+                savedImagesButton.isHidden = true
+                uploadPictureButton.isHidden = false
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        state = .takingImage
         savedImagesButton.setImage(nil, for: .normal)
 
         // Last photo setup
@@ -74,28 +107,114 @@ class ImageSelectorViewController: UIViewController {
         videoPreviewLayer.connection?.videoOrientation = .portrait
         view.layer.addSublayer(videoPreviewLayer)
         session.startRunning()
-
-        photoOutput.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         videoPreviewLayer.frame = view.frame
+        view.bringSubview(toFront: photoPreviewImageView)
         view.bringSubview(toFront: controlsContainer)
     }
 
     // MARK: - Actions
 
     @IBAction func takeImageButtonPressed(_ sender: UIButton) {
+        photoOutput.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
     }
 
     @IBAction func savedImagesButtonPressed(_ sender: UIButton) {
+        presentCamera(withSource: .photoLibrary, delegate: self)
+    }
+
+    @IBAction func uploadPicture(_ sender: UIButton) {
+        if photoPreviewImageView.contentMode == .scaleAspectFill {
+            if let image = photoPreviewImageView.image {
+                presentCropController(withImage: image)
+            }
+        } else {
+            print("Upload")
+        }
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
+        if state == .takingImage {
+            dismiss(animated: true, completion: nil)
+        } else {
+            state = .takingImage
+        }
+    }
+
+    // MARK: - Helpers
+
+    fileprivate func captured(image: UIImage?, error: Error?) {
+        if let error = error {
+            SentryManager.shared.sendEvent(withError: error)
+        } else if let image = image {
+            state = .tookImage
+            DispatchQueue.main.async {
+                self.photoPreviewImageView.image = image
+            }
+        }
+    }
+
+    fileprivate func presentCropController(withImage image: UIImage) {
+        let cropVc = TOCropViewController(image: image)
+        cropVc.imageCropFrame = CGRect(x: 0, y: 0, width: 343, height: 257)
+        cropVc.aspectRatioLockEnabled = true
+        cropVc.resetAspectRatioEnabled = false
+        cropVc.delegate = self
+
+        present(cropVc, animated: true, completion: nil)
     }
 }
 
 extension ImageSelectorViewController: AVCapturePhotoCaptureDelegate {
+
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        if let buffer = photoSampleBuffer, let data = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: buffer, previewPhotoSampleBuffer: nil), let image = UIImage(data: data) {
+            captured(image: image, error: nil)
+        } else if let error = error {
+            captured(image: nil, error: error)
+        }
+    }
+
+    @available(iOS 11.0, *)
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let data = photo.fileDataRepresentation() {
+            captured(image: UIImage(data: data), error: nil)
+        } else {
+            captured(image: nil, error: error)
+        }
+    }
+
+}
+
+extension ImageSelectorViewController: TOCropViewControllerDelegate {
+
+    func cropViewController(_ cropViewController: TOCropViewController, didCropToImage image: UIImage, rect cropRect: CGRect, angle: Int) {
+        cropViewController.dismiss(animated: true, completion: nil)
+        
+        photoPreviewImageView.contentMode = .scaleAspectFit
+        photoPreviewImageView.image = image
+    }
+
+    func cropViewController(_ cropViewController: TOCropViewController, didFinishCancelled cancelled: Bool) {
+        if cancelled {
+            cropViewController.dismiss(animated: true, completion: nil)
+            state = .takingImage
+        }
+    }
+
+}
+
+extension ImageSelectorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        picker.dismiss(animated: true, completion: nil)
+
+        state = .choseImageFromPicker
+        presentCropController(withImage: chosenImage)
+    }
+
 }
---
+++ The Wave/UIViewController+PhotoPermissions.swift
@@ -0,0 +1,46 @@
+//
+//  UIViewController+PhotoPermissions.swift
+//  The Wave
+//
+//  Created by Andrew Robinson on 7/30/17.
+//  Copyright © 2017 XYello, Inc. All rights reserved.
+//
+
+import AVFoundation
+
+extension UIViewController {
+
+    func presentCamera(withSource type: UIImagePickerControllerSourceType, delegate: UIImagePickerControllerDelegate & UINavigationControllerDelegate) {
+
+        if type == .photoLibrary || UIImagePickerController.isSourceTypeAvailable(.camera) {
+
+            let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = delegate
+            imagePicker.sourceType = type
+
+            if status == .notDetermined {
+                if type != .photoLibrary {
+                    AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { granted in
+                        if granted {
+                            self.present(imagePicker, animated: true, completion: nil)
+                        }
+                    })
+                } else {
+                    present(imagePicker, animated: true, completion: nil)
+                }
+            } else if status == .authorized {
+                present(imagePicker, animated: true, completion: nil)
+            } else {
+                if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            }
+        }
+
+    }
+    
+}
