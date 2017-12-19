6bc987b5f51fb9f8570d72d22d194e20eea8ff46
Andrew Robinson <sirarkimedes@gmail.com>
2017-12-19 02:22:32 -0600

    Animate filter display

--- The Wave/Filters/FilterViewController.swift
+++ The Wave/Filters/FilterViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class FilterViewController: UIViewController {
 
     @IBOutlet weak var modelImageView: UIImageView!
+    @IBOutlet weak var modelLabel: UILabel!
     @IBOutlet weak var radiusView: UIView!
-
+    @IBOutlet weak var dashView: UIView!
+    
     @IBOutlet weak var sliderView: SelectionSliderView! {
         didSet {
             sliderView.delegate = self
@@ -22,6 +24,8 @@ class FilterViewController: UIViewController {
     @IBOutlet weak var mileageSlider: UISlider!
     @IBOutlet weak var mileageLabel: UILabel!
     @IBOutlet weak var mileageContainer: UIView!
+    
+    @IBOutlet weak var modelImageViewWidthConstraint: NSLayoutConstraint!
 
     private var sliderItems = [JeepModel.wranglerJK.shortDescription,
                                JeepModel.wranglerTJ.shortDescription,
@@ -52,7 +56,16 @@ class FilterViewController: UIViewController {
         view.backgroundColor = .waveDarkBlue
         
         radiusView.roundCorners()
-        modelImageView.image = Jeep(withType: filter.model).image
+        radiusView.backgroundColor = #colorLiteral(red: 0.01647708192, green: 0.624725759, blue: 1, alpha: 1).withAlphaComponent(0.25)
+        radiusView.addBorder(withWidth: 2.0, color: #colorLiteral(red: 0.01647708192, green: 0.624725759, blue: 1, alpha: 1))
+        dashView.backgroundColor = .clear
+        modelImageView.image = Jeep(withType: filter.model).image?.withRenderingMode(.alwaysTemplate)
+        modelImageView.tintColor = .white
+        modelLabel.textColor = .white
+
+        var string = filter.model.shortDescription
+        if filter.model == .all { string = "All" }
+        modelLabel.text = string
 
         mileageSlider.minimumTrackTintColor = .waveBrightRed
         mileageSlider.maximumTrackTintColor = .white
@@ -61,6 +74,9 @@ class FilterViewController: UIViewController {
         mileageSlider.minimumValue = Float(filter.minimumRadius)
         mileageSlider.value = Float(filter.radius)
         mileageSlider.addTarget(self, action: #selector(sliderValueChange(slider:)), for: .valueChanged)
+
+        modelImageViewWidthConstraint.constant = sizeOfModelImage(for: mileageSlider.value)
+        radiusView.alpha = alphaOfRadius(for: mileageSlider.value)
 
         mileageLabel.textColor = .white
         mileageLabel.text = milesString()
@@ -78,6 +94,10 @@ class FilterViewController: UIViewController {
     @objc func sliderValueChange(slider: UISlider) {
         filter.radius = Int(slider.value)
         mileageLabel.text = milesString()
+
+        modelImageViewWidthConstraint.constant = sizeOfModelImage(for: slider.value)
+        radiusView.alpha = alphaOfRadius(for: slider.value)
+        view.layoutIfNeeded()
     }
 
     // MARK: - Helpers
@@ -88,6 +108,24 @@ class FilterViewController: UIViewController {
         } else {
             return "\(filter.radius) miles"
         }
+    }
+
+    private func sizeOfModelImage(for value: Float) -> CGFloat {
+        if value >= Float(filter.maximumRadius - filter.maximumRadius / 5) {
+            return CGFloat((filter.maximumRadius / 5) - 10)
+        } else if value >= Float(filter.maximumRadius / 5) {
+            return CGFloat(Float(filter.maximumRadius) - value - 10)
+        }
+
+        return CGFloat(filter.maximumRadius - (filter.maximumRadius / 5) - 10)
+    }
+
+    private func alphaOfRadius(for value: Float) -> CGFloat {
+        if value <= Float(filter.maximumRadius / 6) {
+            return CGFloat(value / Float(filter.maximumRadius / 6))
+        }
+
+        return 1.0
     }
 
 }
@@ -105,7 +143,11 @@ extension FilterViewController: SelectionSliderDelegate {
     }
 
     func sliderSelectionDidUpdate(atIndex index: Int) {
+        modelLabel.text = sliderItems[index]
+
         filter.model = JeepModel.enumFrom(shortDescription: sliderItems[index])
-        modelImageView.image = Jeep(withType: filter.model).image
+        UIView.transition(with: modelImageView, duration: 0.25, options: .transitionFlipFromTop, animations: {
+            self.modelImageView.image = Jeep(withType: self.filter.model).image?.withRenderingMode(.alwaysTemplate)
+        }, completion: nil)
     }
 }
